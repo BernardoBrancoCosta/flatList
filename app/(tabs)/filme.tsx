248ef80ee23f8b9a5354aaffdc2cf92d992f5e32
BernardoBC <Bernardo.costa1@alunos.sc.senac.br>
2026-08-20 16:45:36 -0300

    novoConteudo

---
+++ app/(tabs)/filme.tsx
@@ -0,0 +1,142 @@
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+
+
+export default function HomeScreen() {
+  return (
+    <View style={styles.parent}>
+        <View style={styles.window}>
+            <View style={styles.header}>
+                <View style={styles.title}>
+                  Título
+                </View>
+            </View>
+            <View style={styles.body}>
+                <View style={styles.options}>
+                  <View style={styles.option}>1</View>
+                  <View style={styles.option}>2</View>
+                  <View style={styles.watch}>play.png</View>
+                  <View style={styles.option}>4</View>
+                  <View style={styles.option}>5</View>
+                </View>
+                <View style={styles.watchSelection}>
+                  <View style={styles.seasons}></View>
+                  <View style={styles.episodes}></View>
+                </View>
+            </View>
+            <View style={styles.footer}>
+
+            </View>
+        </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  parent: {
+    flex: 1,
+    backgroundColor: '#312e2e',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    overflow: 'scroll',
+    fontFamily: 'arial'
+  },
+  window: {
+    flex: 1,
+    backgroundColor: '#000000',
+    minHeight: 1600,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    width: '75%',
+  },
+  header: {
+    flex: 5,
+    backgroundColor: '#000000',
+    width: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center'
+  },
+    title: {
+      flex: .1,
+      backgroundColor: '#8b3939',
+      width: 'auto',
+      display: 'flex',
+      marginTop: 10,
+      justifyContent: 'center',
+      fontSize: '5vh',
+      },
+  body: {
+    flex: 8,
+    backgroundColor: '#2b2121',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+    options: {
+      flex: .2,
+      width: '100%',
+      backgroundColor: '#242222',
+      borderColor: '#8b2626',
+      borderBottomWidth: '3px',
+      borderTopWidth: '3px',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+    },
+      option: {
+        flex: 1,
+        height: '50%',
+        width: 'auto',
+        backgroundColor: '#5e565670',
+        border: '1px solid #ffffff',
+        margin: '10px',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+      watch: {
+        flex: 3,
+        height: '50%',
+        backgroundColor: '#ffffff67',
+        border: '1px solid #ffffff',
+        margin: '10px',
+        borderRadius: 50,
+        justifyContent: 'center',
+        alignItems: 'center'
+      },
+    watchSelection: {
+      flex: .8,
+      width: '100%',
+      backgroundColor: '#5e565670',
+      display: 'flex',
+      alignItems: 'center'
+    },
+      seasons: {
+        flex: .2,
+        width: '90%',
+        backgroundColor: '#5e565670',
+        borderBottomWidth: '3px',
+        borderColor: '#8b2626'
+
+      },
+      episodes: {
+        flex: .8,
+        width: '90%',
+        backgroundColor: '#5e565670',
+      },
+  footer: {
+    flex: 1,
+    backgroundColor: '#242222',
+    borderTopWidth: '3px',
+    borderColor: '#8b3939',
+    width: '100%',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
+});
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import {View, StyleSheet} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
 
 const categorias = [
   {
@@ -10,7 +10,7 @@ const categorias = [
       { id: "1c", titulo: "Barbie", cor: "#0f3460" },
       { id: "1d", titulo: "Poor Things", cor: "#533483" },
       { id: "1e", titulo: "Saltburn", cor: "#2b2d42" },
-    ],
+    ]
   },
   {
     id: "2",
@@ -20,7 +20,7 @@ const categorias = [
       { id: "2b", titulo: "Missão Impossível", cor: "#162447" },
       { id: "2c", titulo: "Top Gun", cor: "#1f4068" },
       { id: "2d", titulo: "Mad Max", cor: "#1b262c" },
-    ],
+    ]
   },
   {
     id: "3",
@@ -29,7 +29,7 @@ const categorias = [
       { id: "3a", titulo: "Superbad", cor: "#2d132c" },
       { id: "3b", titulo: "The Grand Budapest", cor: "#1c3334" },
       { id: "3c", titulo: "Knives Out", cor: "#2c003e" },
-    ],
+    ]
   },
   {
     id: "4",
@@ -38,7 +38,7 @@ const categorias = [
       { id: "4a", titulo: "Free Solo", cor: "#0d0d0d" },
       { id: "4b", titulo: "The Social Dilemma", cor: "#001011" },
       { id: "4c", titulo: "My Octopus Teacher", cor: "#002b36" },
-    ],
+    ]
   },
   {
     id: "5",
@@ -47,18 +47,80 @@ const categorias = [
       { id: "5a", titulo: "Hereditary", cor: "#200122" },
       { id: "5b", titulo: "Midsommar", cor: "#190a05" },
       { id: "5c", titulo: "Get Out", cor: "#0a0a0a" },
-    ],
+    ]
   },
 ];
 
 
 export default function App() {
-    return (
+  return (
+    <View style={styles.body}>
+        <FlatList
+        data={categorias}
+        keyExtractor={item => item.id}
+        renderItem={renderCategoria}
+        />
+    </View>
+   );
+}
 
-        <View></View>
-    );
+function renderCategoria({item}:{item:any}){
+  return(
+    <View style={styles.categorias}>
+      {item.titulo}:
+        <FlatList style={styles.row}
+        data={item.filmes}
+        keyExtractor={item => item.id}
+        renderItem={renderFilmes}
+        />
+    </View>
+  )
+}
+
+function renderFilmes({item}:{item:any}){
+  return(
+    <View style={[styles.filmes, {backgroundColor:item.cor}]}>
+        {item.titulo}
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
-       
+  body:{
+    flex: 1,
+    justifyContent: 'space-around',
+    backgroundColor: '#000000',
+    fontFamily: 'helvetica',
+  },
+  categorias:{
+    flexDirection: 'column',
+    backgroundColor: '#1b1515',
+    color: '#f55c5c',
+    borderColor: '#8b2626',
+    borderBottomWidth: 2,
+    textAlign: 'left',
+    fontSize: 20,
+    padding: 10,
+  },
+  row:{
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff00',
+    overflow: 'scroll',
+    },
+  filmes:{
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+    textAlign: 'center',
+    fontWeight: 'bold',
+    backgroundColor: '#ff9696',
+    color: '#ffdfdf',
+    borderColor: '#8b3939',
+    borderRadius: 10,
+    borderWidth: 2,
+    fontSize: 20,
+    height: 300,
+    width: 300,
+    margin: 15,
+  }
 });
